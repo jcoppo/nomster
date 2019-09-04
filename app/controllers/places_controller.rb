65bf3d9de331d5742f5d02a632a1f33ed0ed33d2
“Jayson <jaysoncoppo@gmail.com>
2019-09-04 06:04:12 +0000

    create form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,9 +1,16 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.paginate(page: params[:page], per_page: 2)
+    @places = Place.paginate(page: params[:page], per_page: 3)
     # @places = Place.order("name").page(params[:page], per_page: 3)
+  end
 
+  def new
+    @place = Place.new
+  end
+
+  def create
+    
   end
 
 end
