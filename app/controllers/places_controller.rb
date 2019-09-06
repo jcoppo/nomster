0b03e7d03679f72e292b5f0bc19a6834bb991070
“Jayson <jaysoncoppo@gmail.com>
2019-09-06 04:49:30 +0000

    places show top quote 5perpage

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,7 +3,7 @@ class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @places = Place.paginate(page: params[:page], per_page: 3)
+    @places = Place.paginate(page: params[:page], per_page: 5)
     # @places = Place.order("name").page(params[:page], per_page: 3)
   end
 
